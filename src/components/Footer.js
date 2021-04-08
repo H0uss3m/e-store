c5f3272420cf254afd033f7ce11435d1e8463879
h0uss3m <hossem.benmustapha@gmail.com>
2021-04-08 13:37:47 +0100

    fix index css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,54 @@
 import React from 'react'
 import './Footer.css'
+import logo from '../images/footer-logo.png'
+import { Link } from 'react-router-dom'
+
 const Footer = () => {
-  return <div></div>
+  return (
+    <div className='footer'>
+      <div className='footer-divider'></div>
+      <div className='footer-logo'>
+        <img src={logo} alt='store-logo' />
+      </div>
+      <div className='footer-links'>
+        <label>Lien Utiles</label>
+        <ul>
+          <li>
+            <Link>Configurateur</Link>
+          </li>
+          <li>
+            <Link>Notre magasin</Link>
+          </li>
+          <li>
+            <Link>Notice et installation</Link>
+          </li>
+          <li>
+            <Link>Livraison Transport</Link>
+          </li>
+        </ul>
+      </div>
+      <div className='footer-contact'>
+        <label>Nous Contacter</label>
+        <b>Adress :</b>
+        <p>3 rue du champ de verger Allonnes, Pays de la Loire, France</p>
+        <b>Telephone:</b>
+        <p>3 rue du champ de verger Allonnes, Pays de la Loire, France</p>
+      </div>
+      <div className='footer-schedule'>
+        <label>Nous Contacter</label>
+        <b>Lundi a Vendredi</b>
+        <p className='time'>7:30-12:00</p>
+        <p className='time'>13:30-18:00</p>
+        <p>
+          <b>Samedi </b>
+          <br></br>
+          <em>(Fermé les samedis de janvier et février)</em>
+        </p>
+        <p className='time'>9:00-12:00</p>
+      </div>
+      <div className='footer-divider'></div>
+    </div>
+  )
 }
 
 export default Footer
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,92 @@
 import React from 'react'
+import logo from '../../images/1e1fc50768fe089201a9d2f194ad33f1.png'
+import logo1 from '../../images/img-3.png'
+import Footer from '../Footer'
+import Nav from '../Nav'
 import './Home.css'
-const Home = () => {
-  return <div>Home</div>
+
+function Home() {
+  return (
+    <div className='Home'>
+      <Nav />
+      <div className='banner-box'>
+        <div className='main-banner'></div>
+        <div className='banner-cards'>
+          <div className='banner-card'>
+            <img src={logo} alt='logo' />
+          </div>
+          <div className='banner-card'>
+            <img src={logo1} alt='logo' />
+          </div>
+          <div className='banner-card'>
+            <img src={logo} alt='logo' />
+          </div>
+          <div className='banner-card'>
+            <img src={logo1} alt='logo' />
+          </div>
+          <div className='banner-card'>
+            <img src={logo} alt='logo' />
+          </div>
+        </div>
+      </div>
+
+      <div className='body'>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <div>Lorem Ipsem </div>
+        <Footer />
+      </div>
+    </div>
+  )
 }
 
 export default Home
